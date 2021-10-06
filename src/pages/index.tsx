e59d81b80bf67ff935c1634c8685ca2764141d86
Remi Martel <luxor37@Remis-MacBook-Air.local>
2021-10-06 12:00:37 -0400

    blank homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,37 +11,38 @@ import { getAbout, getEducations, getJobs } from 'lib/api'
 export default function Homepage(props) {
 
 	return (
-		<Page>
-			<NextSeo
-				title="Portfolio - Remi Martel"
-				description="I am a backend developper studying in Software and Computer Engineering at UQAM in Montreal."
-				openGraph={{
-					url: 'https://www.rmartel.dev',
-					title: "Portfolio - Remi Martel",
-					description: "I am a backend developper studying in Software and Computer Engineering at UQAM in Montreal.",
-					images: [
-						{
-							url: 'https://www.rmartel.dev/aboutMe.jpg',
-							width: 1161,
-							height: 769,
-							alt: 'AboutSectionBg_WhiteLakeStatePark',
-						},
-						// {
-						// 	url: 'https://www.example.ie/og-image-02.jpg',
-						// 	width: 900,
-						// 	height: 800,
-						// 	alt: 'Og Image Alt Second',
-						// },
-						// { url: 'https://www.example.ie/og-image-03.jpg' },
-						// { url: 'https://www.example.ie/og-image-04.jpg' },
-					],
-					site_name: 'Portfolio - Remi Martel',
-				}}
-			/>
-			{/* <About about={props.about[0]} />
-			<Experience jobs={props.allJobs} />
-			<Education educations={props.allEducations} /> */}
-		</Page>
+    <p>Hello</p>
+		// <Page>
+		// 	<NextSeo
+		// 		title="Portfolio - Remi Martel"
+		// 		description="I am a backend developper studying in Software and Computer Engineering at UQAM in Montreal."
+		// 		openGraph={{
+		// 			url: 'https://www.rmartel.dev',
+		// 			title: "Portfolio - Remi Martel",
+		// 			description: "I am a backend developper studying in Software and Computer Engineering at UQAM in Montreal.",
+		// 			images: [
+		// 				{
+		// 					url: 'https://www.rmartel.dev/aboutMe.jpg',
+		// 					width: 1161,
+		// 					height: 769,
+		// 					alt: 'AboutSectionBg_WhiteLakeStatePark',
+		// 				},
+		// 				// {
+		// 				// 	url: 'https://www.example.ie/og-image-02.jpg',
+		// 				// 	width: 900,
+		// 				// 	height: 800,
+		// 				// 	alt: 'Og Image Alt Second',
+		// 				// },
+		// 				// { url: 'https://www.example.ie/og-image-03.jpg' },
+		// 				// { url: 'https://www.example.ie/og-image-04.jpg' },
+		// 			],
+		// 			site_name: 'Portfolio - Remi Martel',
+		// 		}}
+		// 	/>
+		// 	{/* <About about={props.about[0]} />
+		// 	<Experience jobs={props.allJobs} />
+		// 	<Education educations={props.allEducations} /> */}
+		// </Page>
 	)
 }
 
