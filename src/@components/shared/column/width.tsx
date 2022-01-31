358382c95fb787026cfeaad49b995be68d06c4ff
Remi Martel <remi.martel@nightborn.be>
2022-01-31 01:16:44 -0500

    testing

--- src/@components/shared/column/width.tsx
+++ src/@components/shared/column/width.tsx
@@ -2,7 +2,7 @@ export default function DoNotUse() {
 
     //This is so that tailwind doesn't purge these classes
     return (
-        <div className={`w-1/12 w-2/12 w-3/12 w-4/12 w-5/12 w-6/12 w-7/12 w-8/12 w-9/12 w-10/12 w-11/12`}>
+        <div className={``}>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,44 @@
 module.exports = {
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/@components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        'accent-1': '#FAFAFA',
-        'accent-2': '#EAEAEA',
-        'accent-7': '#333',
-        success: '#0070f3',
-        cyan: '#79FFE1',
-      },
-      spacing: {
-        28: '7rem',
-      },
-      letterSpacing: {
-        tighter: '-.04em',
-      },
-      lineHeight: {
-        tight: 1.2,
-      },
-      fontSize: {
-        '5xl': '2.5rem',
-        '6xl': '2.75rem',
-        '7xl': '4.5rem',
-        '8xl': '6.25rem',
-      },
-      boxShadow: {
-        small: '0 5px 10px rgba(0, 0, 0, 0.12)',
-        medium: '0 8px 30px rgba(0, 0, 0, 0.12)',
-      },
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	// purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/@components/**/*.{js,ts,jsx,tsx}'],
+	purge: {
+		content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/@components/**/*.{js,ts,jsx,tsx}'],
+		options: {
+			safelist: ['w-1/12', 'w-2/12', 'w-3/12', 'w-4/12', 'w-5/12', 'w-6/12', 'w-7/12', 'w-8/12', 'w-9/12', 'w-10/12', 'w-11/12']
+		}
+	},
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				'accent-1': '#FAFAFA',
+				'accent-2': '#EAEAEA',
+				'accent-7': '#333',
+				success: '#0070f3',
+				cyan: '#79FFE1',
+			},
+			spacing: {
+				28: '7rem',
+			},
+			letterSpacing: {
+				tighter: '-.04em',
+			},
+			lineHeight: {
+				tight: 1.2,
+			},
+			fontSize: {
+				'5xl': '2.5rem',
+				'6xl': '2.75rem',
+				'7xl': '4.5rem',
+				'8xl': '6.25rem',
+			},
+			boxShadow: {
+				small: '0 5px 10px rgba(0, 0, 0, 0.12)',
+				medium: '0 8px 30px rgba(0, 0, 0, 0.12)',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 }
