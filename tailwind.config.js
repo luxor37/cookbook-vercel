7fbac829dbe4bd30bd235841f219a1cb666867d0
Remi Martel <remi.martel@nightborn.be>
2022-02-01 01:19:46 -0500

    fixing columns

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,11 @@ module.exports = {
 	purge: {
 		content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/@components/**/*.{js,ts,jsx,tsx}'],
 		options: {
-			safelist: ['w-1/12', 'w-2/12', 'w-3/12', 'w-4/12', 'w-5/12', 
-			'w-6/12', 'w-7/12', 'w-8/12', 'w-9/12', 'w-10/12', 'w-11/12']
-		}
+			safelist: ['w-1/12', 'w-2/12', 'w-3/12', 'w-4/12', 'w-5/12',
+			  'w-6/12', 'w-7/12', 'w-8/12', 'w-9/12', 'w-10/12', 'w-11/12',
+			  'md:w-1/12', 'md:w-2/12', 'md:w-3/12', 'md:w-4/12', 'md:w-5/12',
+			  'md:w-6/12', 'md:w-7/12', 'md:w-8/12', 'md:w-9/12', 'md:w-10/12', 'md:w-11/12']
+		  }
 	},
 	darkMode: false, // or 'media' or 'class'
 	theme: {
